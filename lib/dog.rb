651467fa98e0f43f7e55e57a10f4220aff6cc7dc
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-11-24 00:33:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+class Dog 
+  def name(fido)
+    @name = "fido"
+  end
+  
+  def name 
+    @name
+  end
+end
+
+
+fido = Dog.new ("fido")
+fido.name
+
+def bark
+  bark = "woof!"
+  
+
+end
+
+fido = Dog.new ("fido")
+
+fido.bark
+
